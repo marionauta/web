21dda6a03d970c344a76ff41faf68acda2b3fdfc
Mario Nachbaur <marionauta@users.noreply.github.com>
2023-12-18 14:59:12 +0100

    feat: first commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+  // Copy the `assets` directory to the compiled site folder
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/*.json");
+  eleventyConfig.addPassthroughCopy("./src/*.txt");
+
+  return {
+    dir: {
+      input: "./src",
+      output: "./public",
+    },
+    passthroughFileCopy: true,
+  };
+};
