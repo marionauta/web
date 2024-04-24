824247c2ee3ddc15a1e7dad353267a0bb06c79e6
Mario Nachbaur <marionauta@users.noreply.github.com>
2024-04-24 19:09:29 +0200

    feat(visitsevilla): add basic landing

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   // Copy the `assets` directory to the compiled site folder
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/*.json");
+  eleventyConfig.addPassthroughCopy("./src/**/*.css");
   eleventyConfig.addPassthroughCopy("./src/*.txt");
 
   return {
